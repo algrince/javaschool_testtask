f205048694f4eac3c5fb96154736a3aaaa50da4f
Ekaterina Mitiashkina <algrince@gmail.com>
2023-01-19 13:00:08 +0100

    fix calculator validation

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -11,8 +11,43 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
-        // TODO: Implement the logic here
+
+        if (validate(statement) == false) {
+            return null;
+        }
         return "";
+    }
+
+    public boolean validate(String statement) {
+        char openedBracket = '(';
+        char closedBracket = ')';
+        char coma = ',';
+        boolean validated = true;
+        boolean insideBracket = false;
+
+        if (statement == null || statement.isEmpty() || 
+        statement.matches(".*[^0-9]{2,}.*") || statement.indexOf(coma) != -1) {
+            return false;
+        }
+
+        for (int i=0; i < statement.length(); i++) {
+            if (statement.charAt(i) == openedBracket) {
+                validated = false;
+                insideBracket = true; 
+            } else if (statement.charAt(i) == closedBracket) {
+                if (insideBracket == false) {
+                    validated = false;
+                    break;
+                } else {
+                validated = true;
+            }
+        }
+        }
+
+        if (insideBracket == true) {
+            validated = false;
+        }
+        return validated;
     }
 
 }
