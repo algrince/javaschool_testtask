49459d6e60280756ccecfbd4f5a7748d48786e26
Ekaterina Mitiashkina <algrince@gmail.com>
2023-01-20 13:50:23 +0100

    add operate method in calculator

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -11,41 +11,45 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
+        String plusSign = "+";
+        String minusSign = "-";
+        String timesSign = "*";
+        String divisionSign = "/";
 
         if (validate(statement) == false) {
             return null;
         }
-        while (statement.contains("(") || statement.contains("*") || statement.contains("/")
-            || statement.contains("+") || statement.contains("-")) {
+        while (statement.contains("(") || statement.contains(timesSign) || statement.contains(divisionSign)
+            || statement.contains(plusSign) || statement.contains(minusSign)) {
             if (statement.contains("(")) {
                 String newStatement = statement.substring(statement.indexOf("(") + 1, statement.indexOf(")"));
                 String bracketsNumber = evaluate(newStatement);
                 statement = statement.replaceFirst("\\(([^\\)]+)\\)", bracketsNumber);
             }
-            if (statement.contains("*") && (statement.contains("/"))) {
-                if (statement.indexOf("*") < statement.indexOf("/")) {
-                    statement = multiply(statement);
-                    statement = divide(statement);
+            if (statement.contains(timesSign) && (statement.contains(divisionSign))) {
+                if (statement.indexOf(timesSign) < statement.indexOf(divisionSign)) {
+                    statement = operate(statement, timesSign);
+                    statement = operate(statement, divisionSign);
                 } else {
-                    statement = divide(statement);
-                    statement = multiply(statement);
+                    statement = operate(statement, divisionSign);
+                    statement = operate(statement, timesSign);
                 }
-            } else if (statement.contains("*")) {
-                statement = multiply(statement);
-            } else if (statement.contains("/")) {
-                statement = divide(statement);
-            } else if (statement.contains("+") && statement.contains("-")) {
-                if (statement.indexOf("+") < statement.indexOf("-")) {
-                    statement = sum(statement);
-                    statement = sub(statement);
+            } else if (statement.contains(timesSign)) {
+                statement = operate(statement, timesSign);
+            } else if (statement.contains(divisionSign)) {
+                statement = operate(statement, divisionSign);
+            } else if (statement.contains(plusSign) && statement.contains(minusSign)) {
+                if (statement.indexOf(plusSign) < statement.indexOf(minusSign)) {
+                    statement = operate(statement, plusSign);
+                    statement = operate(statement, minusSign);
                 } else {
-                    statement = sub(statement);
-                    statement = sum(statement);
+                    statement = operate(statement, minusSign);
+                    statement = operate(statement, plusSign);
                 }
-            } else if (statement.contains("-")) {
-                statement = sub(statement);
-            } else if (statement.contains("+")) {
-                statement = sum(statement);
+            } else if (statement.contains(minusSign)) {
+                statement = operate(statement, minusSign);
+            } else if (statement.contains(plusSign)) {
+                statement = operate(statement, plusSign);
             }
         }
         double result = Double.parseDouble(statement);
@@ -66,51 +70,40 @@ public class Calculator {
         return number;
     }
 
-    public String multiply(String statement) {
-        String[] dividedStatement = statement.split("\\*");
+    public String operate(String statement, String operator) {
+        // Find numbers around the operator 
+        String regexOperator = "\\" + operator;
+        String[] dividedStatement = statement.split(regexOperator);
         String left = dividedStatement[0];
         String leftNumber = getLeftNumber(left);
         String right = dividedStatement[1];
         String rightNumber = getRightNumber(right);
-        double result = Double.parseDouble(leftNumber) * Double.parseDouble(rightNumber);
-        String toReplace = leftNumber + "*" + rightNumber;
-        statement = statement.replace(toReplace, Double.toString(result));
-        return statement;
-    }
-    public String divide(String statement) {
-        String[] dividedStatement = statement.split("\\/");
-        String right = dividedStatement[1];
-        String rightNumber = getRightNumber(right);
-        if (Integer.parseInt(rightNumber, 0, 0, 0) == 0) {
-            return null;
-        } else {
-            String left = dividedStatement[0];
-            String leftNumber = getLeftNumber(left);
-            double result = Double.parseDouble(leftNumber) / Double.parseDouble(rightNumber);
-            String toReplace = leftNumber + "/" + rightNumber;
-            statement = statement.replace(toReplace, Double.toString(result));
+
+        // Make an operation and replace the expression with result
+        double result = 0.0;
+        String toReplace = "";
+
+        switch (operator) {
+            case "+":
+                result = Double.parseDouble(leftNumber) + Double.parseDouble(rightNumber);
+                toReplace = leftNumber + "+" + rightNumber;
+                break;
+            case "-":
+                result = Double.parseDouble(leftNumber) - Double.parseDouble(rightNumber);
+                toReplace = leftNumber + "-" + rightNumber;
+                break;
+            case "*":
+                result = Double.parseDouble(leftNumber) * Double.parseDouble(rightNumber);
+                toReplace = leftNumber + "*" + rightNumber;
+                break;
+            case "/":
+                if (Integer.parseInt(rightNumber, 0, 0, 0) == 0) {
+                    return null;
+                } else {
+                    result = Double.parseDouble(leftNumber) / Double.parseDouble(rightNumber);
+                    toReplace = leftNumber + "/" + rightNumber;
+                }
         }
-        return statement;
-    }
-    public String sum(String statement) {
-        String[] dividedStatement = statement.split("\\+");
-        String left = dividedStatement[0];
-        String leftNumber = getLeftNumber(left);
-        String right = dividedStatement[1];
-        String rightNumber = getRightNumber(right);
-        double result = Double.parseDouble(leftNumber) + Double.parseDouble(rightNumber);
-        String toReplace = leftNumber + "+" + rightNumber;
-        statement = statement.replace(toReplace, Double.toString(result));
-        return statement;
-    }
-    public String sub(String statement) {
-        String[] dividedStatement = statement.split("\\-");
-        String left = dividedStatement[0];
-        String leftNumber = getLeftNumber(left);
-        String right = dividedStatement[1];
-        String rightNumber = getRightNumber(right);
-        double result = Double.parseDouble(leftNumber) - Double.parseDouble(rightNumber);
-        String toReplace = leftNumber + "-" + rightNumber;
         statement = statement.replace(toReplace, Double.toString(result));
         return statement;
     }
@@ -119,7 +112,10 @@ public class Calculator {
         String number = ""; 
         for (int i=left.length() - 1; i >=0 ; i--) {
             char character = left.charAt(i);
-            if (Character.isDigit(character) || character == '.') {
+            if (character == '-') {
+                number = number + character;
+                break;
+            } else if (Character.isDigit(character) || character == '.') {
                 number = number + character;
             } else {
                 break;
@@ -133,32 +129,39 @@ public class Calculator {
     }
 
     public boolean validate(String statement) {
-        char openedBracket = '(';
-        char closedBracket = ')';
         char coma = ',';
-        boolean validated = true;
-        boolean insideBracket = false;
 
         if (statement == null || statement.isEmpty() || 
-        statement.matches(".*[^0-9]{2,}.*") || statement.indexOf(coma) != -1) {
+        statement.matches(".*[^0-9()]{2,}.*") || statement.indexOf(coma) != -1) {
             return false;
         }
+
+        char openedBracket = '(';
+        int openedBracketCounter = 0;
+        int closedBracketCounter = 0;
+        char closedBracket = ')';
+        boolean validated = true;
+        boolean insideBracket = false;
+        
 
         for (int i=0; i < statement.length(); i++) {
             if (statement.charAt(i) == openedBracket) {
                 validated = false;
                 insideBracket = true; 
+                openedBracketCounter++;
             } else if (statement.charAt(i) == closedBracket) {
                 if (insideBracket == false) {
                     validated = false;
                     break;
                 } else {
                 validated = true;
+                insideBracket = false;
+                closedBracketCounter++;
             }
         }
         }
 
-        if (insideBracket == true) {
+        if (insideBracket == true || openedBracketCounter != closedBracketCounter) {
             validated = false;
         }
         return validated;
