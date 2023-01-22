e3b2112de69d3b124f8c568e219e30167abda62e
Ekaterina Mitiashkina <algrince@gmail.com>
2023-01-22 16:37:30 +0100

    add converter

--- src/main/java/com/tsystems/javaschool/tasks/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/tsystems/javaschool/tasks/currencyconverter/CurrencyConverter.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.currencyconverter;
 
+import java.util.Stack;
 import java.text.DecimalFormat;
 
 public class CurrencyConverter {
@@ -12,87 +13,116 @@ public class CurrencyConverter {
      * @return converted value
      */
     public String convert(double dollarToEuroRate, String statement) {
-        String convDollar = "convertToDollar";
-        String convEuro = "convertToEuro";
-        String[] splitDollar = statement.split(convDollar, 2);
-        String[] splitEuro = statement.split(convEuro, 2);
-        String change = "";
-        String operation = "";
+        Stack<String> actionOrder = new Stack<String>();
+        boolean hasParentheses = findParentheses(statement, actionOrder);
+        System.out.println(hasParentheses);
+        System.out.println(actionOrder);
+        String output = unpack(actionOrder, dollarToEuroRate);
+        System.out.println(output);
+        return output;
+    }
+    
+    public boolean findParentheses(String statement, Stack actionOrder) {
+        int indexOpened = statement.indexOf("(");
+        int indexClosed = statement.lastIndexOf(")");
+        if (indexOpened != -1 && indexClosed != -1) {
+            actionOrder.push(statement.substring(0, indexOpened));
+            findOperations(statement.substring(indexOpened + 1, indexClosed), actionOrder);
+            if (String.valueOf(actionOrder.peek()).contains("(")) {
+                boolean innerParentheses = findParentheses(String.valueOf(actionOrder.pop()), actionOrder);
+            }
+            return true;
+        } 
+        findOperations(statement, actionOrder);;
+        return false;
+    }
+    
 
-        if (splitDollar.length != 1) {
-            change = "dollar";
-            System.out.print(splitDollar[1]);
-            String moneyToChange = splitDollar[1].substring(1, splitDollar[1].indexOf(")"));
-            System.out.println("TO CHANGE: " + moneyToChange);
-            String money = convert(dollarToEuroRate, moneyToChange);
-            String currency = checkCurrencyType(money);
-            if (currency.equals("dollar")) {
-                double trimmedNumber = trimNumberFromCurrency(money, currency);
-                double result = trimmedNumber / dollarToEuroRate;
-                return uniteNumberWithCurrency(result, "euro");
-            }
-            else {
-                throw new CannotConvertCurrencyException();
-            }
-            
-        } else if (splitEuro.length != 1) {
-            change = "euro";
-            String moneyToChange = splitEuro[1].substring(1, splitEuro[1].indexOf(")"));
-            String money = convert(dollarToEuroRate, moneyToChange);
-            String currency = checkCurrencyType(money);
-            if (currency.equals("euro")) {
-                double trimmedNumber = trimNumberFromCurrency(money, currency);
-                double result = trimmedNumber * dollarToEuroRate;
-                return uniteNumberWithCurrency(result, "dollar");
-            }
-            else {
-                throw new CannotConvertCurrencyException();
-            }
+    public void findOperations(String statement, Stack actionOrder) {
+        int indexPlus = statement.indexOf("+");
+        int indexMinus = statement.indexOf("-");
+        
+        if (indexPlus != -1) {
+            actionOrder.push(statement.substring(0, indexPlus));
+            actionOrder.push("+");
+            actionOrder.push(statement.substring(indexPlus + 1));
+        } else if (indexMinus != -1) {
+            actionOrder.push(statement.substring(0, indexMinus));
+            actionOrder.push("-");
+            actionOrder.push(statement.substring(indexMinus + 1));
         } else {
-            operation = hasOperations(statement);
-            switch (operation) {
-                case "sum":
-                    String[] sumOperands = statement.split("\\+");
-                    if (checkOperandsType(sumOperands)) {
-                        String currency = checkCurrencyType(sumOperands[0]);
-                        double trimmedSumOperand1 = trimNumberFromCurrency(sumOperands[0], currency);
-                        double trimmedSumOperand2 = trimNumberFromCurrency(sumOperands[1], currency);
-                        double result = trimmedSumOperand1 + trimmedSumOperand2;
-                        return uniteNumberWithCurrency(result, currency);
-                    } else {
-                        throw new CannotConvertCurrencyException();
-                    }
-                case "substraction":
-                    String[] subOperands = statement.split("\\-");
-                    if (checkOperandsType(subOperands)) {
-                        String currency = checkCurrencyType(subOperands[1]);
-                        double trimmedSubOperand1 = 0;
-                        double trimmedSubOperand2 = 0;
-                        if (subOperands.length == 2) {
-                            trimmedSubOperand1 = trimNumberFromCurrency(subOperands[0], currency);
-                            trimmedSubOperand2 = trimNumberFromCurrency(subOperands[1], currency);
-                            trimmedSubOperand2 *= -1; 
-                        } else if (subOperands.length == 3) {
-                            trimmedSubOperand1 = trimNumberFromCurrency(subOperands[1], currency);
-                            trimmedSubOperand1 *= -1;
-                            trimmedSubOperand2 = trimNumberFromCurrency(subOperands[2], currency);
-                            trimmedSubOperand2 *= -1;
-                        }
-                        double result = trimmedSubOperand1 + trimmedSubOperand2;
-                        return uniteNumberWithCurrency(result, currency);
-                    } else {
-                        throw new CannotConvertCurrencyException();
-                    }
-                case "no operation":
-                    String currency = checkCurrencyType(statement);
-                    if (currency.equals("dollar") || currency.equals("euro")) {
-                        return uniteNumberWithCurrency(trimNumberFromCurrency(statement, currency), currency);
-                    } else {
-                        throw new CannotConvertCurrencyException();
-                    }
-            }
+            actionOrder.push(statement);
         }
-        return null;
+    }
+
+    public String unpack(Stack actionOrder, double dollarToEuroRate) {
+        while (!actionOrder.isEmpty()) {
+            String element = String.valueOf(actionOrder.pop());
+                String currency = checkCurrencyType(element);
+                double number = trimNumberFromCurrency(element, currency);
+                String nextElement = "";
+                if (actionOrder.size() >= 1) {
+                    nextElement = String.valueOf(actionOrder.pop());
+                } else {
+                    return uniteNumberWithCurrency(number, currency);
+                }
+
+                if (nextElement.equals("convertToDollar") && currency == "euro") {
+                    actionOrder.push(uniteNumberWithCurrency(number / dollarToEuroRate, "dollar"));
+                } else if (nextElement.equals("convertToEuro") && currency == "dollar") {
+                    actionOrder.push(uniteNumberWithCurrency(number * dollarToEuroRate, "euro"));
+                } else if (nextElement.equals("convertToDollar") || nextElement.equals("convertToEuro")) {
+                    throw new CannotConvertCurrencyException();
+                } else if (nextElement.equals("+")) {
+                    String secondOperand = String.valueOf(actionOrder.pop());
+                    if (secondOperand.contains("convertToDollar") || secondOperand.contains("convertToEuro")) {
+                        secondOperand = convert(dollarToEuroRate, secondOperand);
+                    }
+                    String secondCurrency = checkCurrencyType(secondOperand);
+                    if (currency != secondCurrency) {
+                        throw new CannotConvertCurrencyException();
+                    }
+                    double secondNumber = trimNumberFromCurrency(secondOperand, secondCurrency);
+                    actionOrder.push(uniteNumberWithCurrency(number + secondNumber, secondCurrency));
+                } else if (nextElement.equals("-")) {
+                    String secondOperand = String.valueOf(actionOrder.pop());
+                    if (secondOperand.contains("convertToDollar") || secondOperand.contains("convertToEuro")) {
+                        secondOperand = convert(dollarToEuroRate, secondOperand);
+                    }
+                    String secondCurrency = checkCurrencyType(secondOperand);
+                    if (currency != secondCurrency) {
+                        throw new CannotConvertCurrencyException();
+                    }
+                    double secondNumber = trimNumberFromCurrency(secondOperand, secondCurrency);
+                    actionOrder.push(uniteNumberWithCurrency(secondNumber - number, secondCurrency));
+                }
+            }
+        return "";
+    }
+    
+    public String checkType(String element) {
+        if (element.equals("convertToDollar")) {
+            return "convert dollar";
+        } else if (element.equals("convertToEuro")){
+            return "convert euro";
+        } else if (element.contains("+")) {
+            return "suma";
+        } else if (element.contains("-")) {
+            return "substraction";
+        } else {
+            return "simple number";
+        }
+    }
+
+    public String checkCurrencyType(String currency) {
+        char dollarSign = '$';
+        char euroLastSign = 'o';
+        if (currency.charAt(0) == dollarSign) {
+            return "dollar";
+        } else if (currency.charAt(currency.length() - 1) == euroLastSign) {
+            return "euro";
+        }
+        throw new CannotConvertCurrencyException();
     }
 
     public double trimNumberFromCurrency(String operand, String currency) {
@@ -108,41 +138,12 @@ public class CurrencyConverter {
     public String uniteNumberWithCurrency(double number, String currency) {
         switch (currency) {
             case "dollar":
+                System.out.println("$" + format(number));
                 return "$" + format(number);
             case "euro":
                 return format(number) + "euro";
         }
         throw new CannotConvertCurrencyException();
-    }
-
-    public static boolean checkOperandsType(String[] operands) {
-        return checkCurrencyType(operands[0]) == checkCurrencyType(operands[1]);
-    }
-
-    public static String checkCurrencyType(String currency) {
-        char dollarSign = '$';
-        System.out.println(currency);
-        if (currency.charAt(0) == dollarSign) {
-            return "dollar";
-        } else if (currency.substring(currency.length() - 4).equals("euro")) {
-            return "euro";
-        }
-        throw new CannotConvertCurrencyException();
-    }
-
-    public static String hasOperations(String expression) {
-        int idxMinus = expression.indexOf('-');
-        int idxPlus = expression.indexOf('+');
-        if (idxPlus != -1) {
-            return "sum";
-        }
-        else if (idxMinus != -1) {
-            if (idxMinus == 0 || expression.charAt(idxMinus - 1) == '(' || idxMinus == 1) {
-                return "no operation";
-            }
-            return "substraction";
-        }
-        return "no operation";
     }
 
     public String format(double result) {
--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -15,15 +15,19 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        int InputSize = inputNumbers.size();
-        int rows = getPiramidSize(InputSize);
-        int columns = 2 * rows - 1;
+
+        // Validate elements of the input
         if (inputNumbers.contains(null)) {
             throw new CannotBuildPyramidException();
         } 
+
+        // Calculate number of rows in function of input length
+        int InputSize = inputNumbers.size();
+        int rows = getPiramidSize(InputSize);
+        int columns = 2 * rows - 1;
+
         Collections.sort(inputNumbers);
         int[][] matrix = createMatrixWithZeros(rows, columns);
-
         
         int numberIdx = 0;
         int followingPoint = 0;
@@ -31,7 +35,7 @@ public class PyramidBuilder {
         for (int i = 1; i <= matrix.length; i++) {
             int numberInRowCounter = 0;
             int rowNumber = i;
-            int startingPoint = (int) (columns - rowNumber - (rowNumber - 1)) / 2;
+            int startingPoint = (int) (columns - 2 * rowNumber + 1) / 2;
             followingPoint = startingPoint;
             while (numberInRowCounter < rowNumber) {
                 matrix[rowNumber - 1][followingPoint] = inputNumbers.get(numberIdx);
@@ -45,6 +49,16 @@ public class PyramidBuilder {
     }
 
     public int getPiramidSize(int InputSize) throws CannotBuildPyramidException {
+        /*
+         * R = row size; I = input size
+         * For finding a relation between a size of given array and rows in piramid:
+         * The number of no-zeros in a base row is equal to the number of rows.
+         * The piramid is stacking from 1 to R one by one (there is 1 number in the first row, 2 -- in the second, etc...)
+         * So it is possible to use a formula for the sum of the first natural numbers:
+         * I = R(R+1)/2
+         * To find R:
+         * 2I = R^2 + R ---> R = sqrt(2I + 0.25) - 0.5
+         */
         int rows = 0;
          double rowSize = Math.sqrt(2*InputSize + 0.25) - 0.5;
         if (rowSize % 1 != 0) {
@@ -59,6 +73,7 @@ public class PyramidBuilder {
     }
 
     public int[][] createMatrixWithZeros(int rows, int columns) {
+        // Create a matrix with defined size and fill it with 0
         int[][] matrix = new int[rows][columns];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
